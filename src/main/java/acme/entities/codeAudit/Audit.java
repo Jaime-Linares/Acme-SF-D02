7c3ae558c9f2e6418a53b9c369dc504a56b45cac
IbaiPerez <ibaperfer@alum.us.es>
2024-02-21 12:35:02 +0100

    Task I5-02

--- src/main/java/acme/entities/codeAudit/Audit.java
+++ src/main/java/acme/entities/codeAudit/Audit.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
@@ -18,12 +19,17 @@ import lombok.Setter;
 @Setter
 public class Audit extends AbstractEntity {
 
+	private static final long	serialVersionUID	= 1L;
+
 	@Column(unique = true)
 	@NotBlank
 	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
-	private String	code;
+	private String				code;
 
 	@Past
-	private Date	executionDate;
+	private Date				executionDate;
+
+	@NotNull
+	private AuditType			type;
 
 }
---
+++ src/main/java/acme/entities/codeAudit/AuditType.java
@@ -0,0 +1,6 @@
+
+package acme.entities.codeAudit;
+
+public enum AuditType {
+	Static, Dynamic
+}
